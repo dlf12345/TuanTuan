d75b407bcc46c7365b21ef750522929dbed6782f
dulongfei <2237570058@qq.com>
2023-05-26 11:38:51 +0800

    The six commit

---
+++ src/main/java/com/example/tuantuan/Mapper/CategoryMapper.java
@@ -0,0 +1,7 @@
+package com.example.tuantuan.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.tuantuan.domain.Categoryee;
+
+public interface CategoryMapper extends BaseMapper<Categoryee> {
+}
